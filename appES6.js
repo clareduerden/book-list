3bbf32c4e8b68ef7dc91b9235bb89f28229f88ff
clareduerden <clare.duerden@gmail.com>
2020-06-22 16:40:53 +0100

    Add localStorage functionality to addBookToList

--- appES6.js
+++ appES6.js
@@ -19,6 +19,18 @@ class UI {
     <td><a href="#" class="delete">X</a></td>
     `;
     bookList.appendChild(tr);
+
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    }
+    else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    console.log("Book added to Local Storage!");
   }
 
   deleteBookFromList(target) {
